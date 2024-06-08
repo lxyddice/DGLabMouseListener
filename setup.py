000496e466d7b5fa66e7afa2cb96c0070127701b
nobody-x-j <nobody.x.j.90@gmail.com>
2024-06-09 01:38:58 +0200

    测试安装

---
+++ setup.py
@@ -0,0 +1,12 @@
+#coding:utf8
+from setuptools import setup
+setup(
+    name="DGLabMouseListener2.0",
+    version=2.0,
+    author="N/A",
+    license="Apache",
+    packages=["DGLabMouseListener2.0"],
+    install_requires=[
+        
+    ]
+    )

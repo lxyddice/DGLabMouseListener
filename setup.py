4f93826cc9b5f1a6033a86598d8209d6bcc54329
nobody-x-j <nobody.x.j.90@gmail.com>
2024-06-09 02:17:55 +0200

    添加需要的包

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #coding:utf8
 from setuptools import setup
 setup(
-    name="DGLabMouseListener2.0",
+    name="DGLabMouseListener2",
     version=2.0,
     author="N/A",
     license="Apache",
-    packages=["DGLabMouseListener2.0"],
+    packages=["DGLabMouseListener2"],
     install_requires=[
-        
+        "requests",
+        "websockets",
+        "loguru"
+        "pynput"
     ]
     )
